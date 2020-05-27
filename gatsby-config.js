bb678d33261d1e2660c09124c26149caa0bab642
Juciano de Carvalho Barbosa <juciano.barbosa@sensedia.com>
2020-05-27 00:25:27 -0300

    start project

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,31 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions:[".mdx", ".md"],
+        gatsbyRemarkPlugins: [ `gatsby-remark-images` ],
+        plugins: [ `gatsby-remark-images` ],
+        defaultLayouts: {
+          pages: `${__dirname}/src/components/layout.js`,
+          default:`${__dirname}/src/components/layout.js`
+        }
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
